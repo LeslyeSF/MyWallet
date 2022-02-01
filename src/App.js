e72cd1fcf5b8eed18282a4e40889667ddfcccad4
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-02-01 17:25:32 -0300

    initial settings

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={}/>
+        <Route path="/signUp" element={}/>
+        <Route path="/home" element={}/>
+        <Route path="/signUp" element={}/>
+        <Route path="/signUp" element={}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+
+let root = document.querySelector(".root");
+ReactDOM.render(<App/>, root);
