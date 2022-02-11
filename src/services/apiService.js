6d69d87be6f6758b5bff2cf080163acf57fbb702
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-02-11 03:36:15 -0300

    add link heroku

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const BASE_URL="https://mywallet80.herokuapp.com";
+
 function createConfig(token){
   const config = {
     headers:{
@@ -9,37 +11,37 @@ function createConfig(token){
   return config;
 }
 export function login(body){
-  const promise = axios.post("http://localhost:5000/login", body);
+  const promise = axios.post(`${BASE_URL}/login`, body);
   return promise;
 }
 export function signUp(body){
-  const promise = axios.post("http://localhost:5000/register", body);
+  const promise = axios.post(`${BASE_URL}/register`, body);
   return promise;
 }
 export function getData(token){
   const config = createConfig(token);
   
-  const promise = axios.get("http://localhost:5000/data", config);
+  const promise = axios.get(`${BASE_URL}/data`, config);
 
   return promise;
 }
 export function submitRecord(body,token){
   const config = createConfig(token);
-  const promise = axios.post("http://localhost:5000/records",body,config);
+  const promise = axios.post(`${BASE_URL}/records`,body,config);
   return promise;
 }
 export function deleteSession(token){
   const config = createConfig(token);
-  const promise = axios.delete("http://localhost:5000/endsession",config);
+  const promise = axios.delete(`${BASE_URL}/endsession`,config);
   return promise;
 }
 export function deleteRecord(token,id){
   const config = createConfig(token);
-  const promise = axios.delete(`http://localhost:5000/deleterecord?idRecord=${id}`, config);
+  const promise = axios.delete(`${BASE_URL}/deleterecord?idRecord=${id}`, config);
   return promise;
 }
 export function editRecord(body, token){
   const config = createConfig(token);
-  const promise = axios.post("http://localhost:5000/editrecord", body, config);
+  const promise = axios.post(`${BASE_URL}/editrecord`, body, config);
   return promise;
 }
